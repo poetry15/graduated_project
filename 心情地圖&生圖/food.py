86c504e804261941f6e179827a0c8d1a79124386
poetry15 <jimmy147156@gmail.com>
2024-09-23 02:19:59 +0800

    改刷新時間

--- 心情地圖&生圖/food.py
+++ 心情地圖&生圖/food.py
@@ -10,12 +10,7 @@ load_dotenv()
 openai.api_key = os.getenv("openai.api_key")
 
 client = MongoClient(os.getenv("ALTAS_URL"))
-db = client["test"]
-All_Keywords = db["All_Keywords"]
-keywords = All_Keywords.find()[0]['AllKeyWords']
 
-
-current_time = datetime.datetime.now().strftime("%H:%M:%S")
 def get_food_recommendation():
   response = openai.chat.completions.create(
     model="gpt-3.5-turbo",
@@ -31,34 +26,40 @@ def get_food_recommendation():
   text = message_dict["message"]["content"].strip()
   return text
 
-vaild = "False"
-while "False" in vaild:
-  text = get_food_recommendation()
-  response = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are an inspector. Your task is to verify if the input contains valid food names(英文中文皆可). If the input contains exactly two food items, return them in the format '[Food1], [Food2]' (replace 'Food1' and 'Food2' with the actual food names). If any of the items are not food, return False."},
-      {"role": "user", "content": f"Check if this input is correct and return only the food names in the format '[Food1], [Food2]': {text}"}
-      
-      # {"role": "system", "content": "You are an inspector. Your task is to verify if the input contains valid food names(英文中文皆可). If the input contains exactly two food items, return them in the format '[Food1], [Food2]' (replace 'Food1' and 'Food2' with the actual food names). If any of the items are not food or the format is incorrect, return 'False'."},
-      # {"role": "user", "content": f"Check if this input is correct and return only the food names in the format '[Food1], [Food2]': {text}"}
-    ]
-) 
-  message_dict = response.choices[0].to_dict()
-  vaild = message_dict["message"]["content"].strip()
-  print(text)
-  print(vaild)
-vaild = vaild.split(", ")
-
-response = openai.images.generate(
-  model="dall-e-3",
-  prompt=f"生成一張圖片，圖的左半邊是{vaild[0]}，圖的右半邊是{vaild[1]}",
-  size="1792x1024",
-  quality="standard",
-  n=1,
-)
-image_url = response.data[0].url
-urllib.request.urlretrieve(image_url, 'assets\\positive_food.png')
+db = client["test"]
+All_Keywords = db["All_Keywords"]
+with All_Keywords.watch() as stream:
+  for change in stream:
+    if change["operationType"] == "update":
+      keywords = All_Keywords.find()[0]['AllKeyWords']
+      current_time = datetime.datetime.now().strftime("%H:%M:%S")
+      vaild = "False"
+      while "False" in vaild:
+        text = get_food_recommendation()
+        response = openai.chat.completions.create(
+          model="gpt-3.5-turbo",
+          messages=[
+            {"role": "system", "content": "You are an inspector. Your task is to verify if the input contains valid food names(英文中文皆可). If the input contains exactly two food items, return them in the format '[Food1], [Food2]' (replace 'Food1' and 'Food2' with the actual food names). If any of the items are not food, return False."},
+            {"role": "user", "content": f"Check if this input is correct and return only the food names in the format '[Food1], [Food2]': {text}"}
+            
+            # {"role": "system", "content": "You are an inspector. Your task is to verify if the input contains valid food names(英文中文皆可). If the input contains exactly two food items, return them in the format '[Food1], [Food2]' (replace 'Food1' and 'Food2' with the actual food names). If any of the items are not food or the format is incorrect, return 'False'."},
+            # {"role": "user", "content": f"Check if this input is correct and return only the food names in the format '[Food1], [Food2]': {text}"}
+          ]
+        ) 
+        message_dict = response.choices[0].to_dict()
+        vaild = message_dict["message"]["content"].strip()
+        print(text)
+        print(vaild)
+      vaild = vaild.split(", ")
+      response = openai.images.generate(
+        model="dall-e-3",
+        prompt=f"生成一張圖片，圖的左半邊是{vaild[0]}，圖的右半邊是{vaild[1]}",
+        size="1792x1024",
+        quality="standard",
+        n=1,
+      )
+      image_url = response.data[0].url
+      urllib.request.urlretrieve(image_url, 'assets\\positive_food.png')
 
 # response = openai.images.generate(
 #   model="dall-e-3",
