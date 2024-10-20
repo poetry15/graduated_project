69d748e9b774ecc1ddc2c6bdfd425eeea6f24199
shaliy <D1051462@o365.fcu.edu.tw>
2024-10-20 14:26:54 +0800

    test

--- form/static/test.js
+++ form/static/test.js
@@ -316,6 +316,7 @@ function scancode() {
       fetch(url+"/check_scanres", {
         method: 'POST',
         headers: {
+          "Content-Type": "application/json",
           "ngrok-skip-browser-warning": true,
         },
         body: JSON.stringify({
