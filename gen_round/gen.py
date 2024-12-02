4d2dfc64c99f0d5fdb8419b7ccd451b3a7d6de05
shaliy <D1051462@o365.fcu.edu.tw>
2024-12-02 14:52:14 +0800

    no changes

--- gen_round/gen.py
+++ gen_round/gen.py
@@ -367,7 +367,7 @@ def validate_gen():
             "num_samples": 1,
             "guidance_scale": 8.0,
             "negative_prompt": "anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
-            "num_inference_steps": 30,
+            "num_inference_steps": 5,
             "adapter_conditioning_scale": 0.95
         }
     )
