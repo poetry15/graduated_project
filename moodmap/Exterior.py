ab297fc3804a72397970d2c07b673d5ba420e47b
poetry15 <jimmy147156@gmail.com>
2024-12-02 11:59:02 +0800

    修改圖片成唯一檔名

--- moodmap/Exterior.py
+++ moodmap/Exterior.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import base64
 import requests
+import uuid
 import datetime
 
 load_dotenv()
@@ -47,7 +48,7 @@ def imageGenerate(keywords,bucket):
     return None
 
 def upload_image_to_firebase(image_data,bucket):
-  filename = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.png"  # 為圖片生成唯一名稱
+  filename = f"{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}.png"  # 為圖片生成唯一名稱
   # 上傳到 Firebase Storage
   blob = bucket.blob(filename)
   blob.upload_from_string(base64.b64decode(image_data), content_type="image/png")
